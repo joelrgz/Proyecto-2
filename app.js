5971ff451953648d3ea054684a610b2071a1022a
Joel Rodriguez <joelrgz95@gmail.com>
2022-09-22 13:45:47 -0500

    funcion leer agregado

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.getElementById("datos").addEventListener ("submit",crear);
 
 function crear(e){
-    e.preventDefault();
+    //e.preventDefault();
     let infor = []
     nombre = document.getElementById("nombre").value
     correo = document.getElementById("correo").value
@@ -25,3 +25,23 @@ function crear(e){
     console.log("Datos guardados correctamente")
 
 }
+
+function leer(){
+    let infor = JSON.parse(localStorage.getItem("infor"))
+    document.getElementById("tbody").innerHTML = ""
+    for (let i=0; i < infor.length; i++){
+        let nombre = infor[i].nombre
+        let correo = infor[i].correo
+        let numero = infor[i].numero
+
+        document.getElementById("tbody").innerHTML +=
+        `<tr>
+            <td>${nombre}</td>
+            <td>${correo}</td>
+            <td>${numero}</td>
+      </tr>`
+
+    }
+}
+
+leer();
