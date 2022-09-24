7bd64bfdc5bf3dc47995c48eef53280e8d694b02
Joel Rodriguez <joelrgz95@gmail.com>
2022-09-24 16:10:47 -0500

    Final

--- app.js
+++ app.js
@@ -39,17 +39,17 @@ function leer(){
             <td>${nombre}</td>
             <td>${correo}</td>
             <td>${numero}</td>
-            <td><button onclick="eliminar()" class="btn btn-danger">Eliminar</button></td>
-            <td><button onclick="editar('${any}')" class="btn btn-success">Editar</button></td>
+            <td><button onclick="eliminar('${nombre}')" class="btn btn-danger">Eliminar</button></td>
+            <td><button onclick="editar('${correo}')" class="btn btn-success">Editar</button></td>
       </tr>`
-
     }
 }
 
-function editar(any){
+function editar(nombre){
     let infor = JSON.parse(localStorage.getItem("infor"));
     for (let i=0; i<infor.length; i++){
-        if(infor[i].any === any){
+          // infor[i].any -> infor[i].correo
+        if(infor[i].correo === nombre){
             document.getElementById("body").innerHTML =
             `<div class="row">
             <div class="column-md-5">
@@ -58,28 +58,26 @@ function editar(any){
                         <h2>Editar datos<h2>
                     </div>
                     <div class="card-body">
-                        <form>
                             <div class="form-group">
                                 <input type="text" 
                                 id="newNombre" 
                                 class="form-control" 
-                                placeholder="${infor[i].nombre}">
+                                value="${infor[i].nombre}">
                             </div>
                             <div class="form-group">
                                 <input type="email" 
                                 id="newCorreo" 
                                 class="form-control" 
-                                placeholder="${infor[i].correo}"></input>
+                                value="${infor[i].correo}">
                             </div>
                             <div class="form-group">
                                 <input type="tel" 
                                 id="newNumero" 
                                 class="form-control" 
-                                placeholder="${infor[i].numero}">
+                                value="${infor[i].numero}">
                             </div>
-                            <button class="btn btn-success" onclick="editar('${i}')">Editar</button>
-                            <button class="btn btn-primary">Eliminar</button>
-                        </form>
+                            <button class="btn btn-success" onclick="actualizar(${i})">Editar</button>
+                            <button class="btn btn-primary" onclick="vistaPrincipal()">Eliminar</button>
                     </div>
                 </div>`
         }
@@ -87,5 +85,67 @@ function editar(any){
     
 }
 
+function actualizar(i){
+    let infor = JSON.parse(localStorage.getItem("infor"));
+    infor[i].nombre = document.getElementById("newNombre").value;
+    infor[i].correo = document.getElementById("newCorreo").value;
+    infor[i].numero = document.getElementById("newNumero").value;
+    localStorage.setItem("infor", JSON.stringify(infor));
+    vistaPrincipal()
+}
 
+function eliminar(nombre){
+    let infor = JSON.parse(localStorage.getItem("infor"));
+    for(let i=0; i<infor.length; i++){
+        if(infor [i].nombre === nombre){
+        infor.splice(i,1);
+        }
+    }
+    localStorage.setItem("infor",JSON.stringify(infor));
+    leer();
+}
+
+function vistaPrincipal(){
+    document.getElementById("body").innerHTML = `<div class="column-md-5">
+    <div class="card">
+        <div class="card-header">
+            <h2>Ingresa tus datos<h2>
+        </div>
+        <div class="card-body">
+            <form id="datos">
+                <div class="form-group">
+                    <input type="text" id="nombre" class="form-control" placeholder="Ingresa tu nombre">
+                </div>
+                <div class="form-group">
+                    <input type="email" id="correo" class="form-control" placeholder="Ingresa tu correo"></input>
+                </div>
+                <div class="form-group">
+                    <input type="tel" id="numero" class="form-control" placeholder="Ingresa tu número telefónico">
+                </div>
+                <button type="submit" class="btn btn-primary">Guardar</button>
+            </form>
+        </div>
+    </div>
+</div>
+<div class="column-md-6">
+    <table class="table">
+        <thead class="table-striped">
+          <tr>
+            <th scope="col">Nombre</th>
+            <th scope="col">Correo</th>
+            <th scope="col">Teléfono</th>
+          </tr>
+        </thead>
+        <tbody id="tbody">
+          <tr>
+            <td>Mark</td>
+            <td>Me gustan los perros</td>
+            <td>3314768956</td>
+          </tr>
+        </tbody>
+      </table>
+</div>
+</div>`
+leer();
+}
 leer();
